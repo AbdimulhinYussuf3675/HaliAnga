0f443de0b15ba89431b3a73d5b896e5ea64606e5
AbdimulhinYussuf3675 <abdimulhin.yussuf@student.moringaschool.com>
2021-07-29 08:36:53 +0300

    registered profile

--- weatherviz/admin.py
+++ weatherviz/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from weatherviz.models import OpenWeatherCity
+from weatherviz.models import OpenWeatherCity,Profile
 
 
 class OpenWeatherCityAdmin(admin.ModelAdmin):
     list_display = ("id", "open_weather_id", "name", "country")
 
 
+
+admin.site.register(Profile)
 admin.site.register(OpenWeatherCity, OpenWeatherCityAdmin)
